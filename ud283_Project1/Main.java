0321b6b2a4d8d1240aac0c3b8aced4e1780bf265
nalupai <102017422+nalupai@users.noreply.github.com>
2022-07-05 15:39:15 +0200

    Title encoding

--- ud283_Project1/Main.java
+++ ud283_Project1/Main.java
@@ -9,19 +9,37 @@ class Main {
         int maxTitles = 25;
         int availableTitles = 0;
         String[] titles = new String[maxTitles];
-
         File file = new File("titles.txt");
         Scanner scannerFile = new Scanner(file);
 
         while (scannerFile.hasNextLine() && availableTitles < maxTitles) {
             titles[availableTitles] = scannerFile.nextLine();
-            System.out.println(titles[availableTitles]);
             availableTitles++;
         }
 
         Random random = new Random();
         int randomTitleIndex = random.nextInt(availableTitles);
-        System.out.println(randomTitleIndex);
+
+        String title = titles[randomTitleIndex];
+        System.out.println(title);
+
+        char[] answer = new char[title.length()];
+
+        for (int i = 0; i < title.length(); i++) {
+            answer[i] = title.charAt(i);
+            System.out.println(answer[i]);
+        }
+
+        char[] game = new char[title.length()];
+
+        for (int i = 0; i < title.length(); i++) {
+            if (answer[i] != ' ') {
+                game[i] = '_';
+            } else {
+                game[i] = answer[i];
+            }
+            System.out.println(game[i]);
+        }
 
         scannerFile.close();
     }
