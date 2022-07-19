387647f7cbd9103bc52bb2aea2bf8492748af270
nalupai <102017422+nalupai@users.noreply.github.com>
2022-07-19 11:46:23 +0200

    Reading from a file

---
+++ ud283_Project1/OOP solution/Main.java
@@ -0,0 +1,9 @@
+import java.io.FileNotFoundException;
+
+class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        TakeTitle takeTitle = new TakeTitle();
+        System.out.println(takeTitle.title);
+    }
+}
